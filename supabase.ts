cee5908afcfef32e0ebfdc44f2128d762fa0475a
yjgaia <yjdev88@gmail.com>
2024-08-31 12:50:15 +0900

    Update supabase.ts

--- supabase.ts
+++ supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.31.0";
 
-const supabase = createClient(
+export const supabase = createClient(
   Deno.env.get("SUPABASE_URL")!,
   Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
 );
-
-export default supabase;
