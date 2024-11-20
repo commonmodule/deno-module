bfc23dbcc708428a03b3ecd518f9b314ad30cdf3
yjgaia <yjdev88@gmail.com>
2024-11-20 14:40:35 +0900

    Add support for Open Graph and Twitter meta tags in createPage function

--- page.ts
+++ page.ts
@@ -57,17 +57,49 @@ export function el<S extends DomSelector>(
 
 export function createPage(options: {
   title: string;
+  description?: string;
+  coverImageURL?: string;
   jsFiles?: string[];
   cssFiles: string[];
   gtagId?: string;
   viewTransition?: boolean;
+  twitterHandle?: string;
 }, ...children: string[]): string {
+  let ogTags =
+    `<meta property="og:type" content="website">\n<meta property="og:title" content="${options.title}">`;
+  ogTags += options.coverImageURL
+    ? `\n<meta property="og:image" content="${options.coverImageURL}">`
+    : "";
+  ogTags += options.description
+    ? `\n<meta property="og:description" content="${options.description}">`
+    : "";
+
+  let twitterTags =
+    `<meta name="twitter:card" content="summary">\n<meta name="twitter:title" content="${options.title}">`;
+  twitterTags += options.coverImageURL
+    ? `\n<meta name="twitter:image" content="${options.coverImageURL}">`
+    : "";
+  twitterTags += options.description
+    ? `\n<meta name="twitter:description" content="${options.description}">`
+    : "";
+  twitterTags += options.twitterHandle
+    ? `\n<meta name="twitter:site" content="@${options.twitterHandle}">`
+    : "";
+
   return `<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge, chrome=1">
+  ${
+    options.description
+      ? `<meta name="description" content="${options.description}">`
+      : ""
+  }
+  ${ogTags}
+  ${twitterTags}
+  <meta name="robots" content="index, follow">
   ${
     options.viewTransition
       ? '<meta name="view-transition" content="same-origin" />'
